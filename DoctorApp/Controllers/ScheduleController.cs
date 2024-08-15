bda6c020ea86ea586fc13b3e311052f1281f9f85
Husnain Memon <husnainraza61@gmail.com>
2024-08-15 04:54:05 -0700

    Work Complete

--- DoctorApp/Controllers/ScheduleController.cs
+++ DoctorApp/Controllers/ScheduleController.cs
@@ -81,5 +81,98 @@ namespace DoctorApp.Controllers
             return Json(new { data = 0 });
         }
 
+
+        public ActionResult EditSchedule(int id)
+        {
+            var row = db.BrowseScheduleByID_sp(id).FirstOrDefault();
+            ScheduleViewModel model1 = new ScheduleViewModel()
+            {
+                ScheduleID = row.ID,
+                Image = row.Image,
+                Days = row.Days,
+                DoctorName = row.DoctorName,
+                DepartmentsName = row.DepartmentsName,
+                StartTime = Convert.ToDateTime(row.AvailableTime),
+                EndTime = Convert.ToDateTime(row.EndTime),
+                Status = row.Status
+
+            };
+            return View(model1);
+        }
+
+
+
+        [HttpPost]
+        public JsonResult EditSchedule(Schedule s)
+        {
+            if (Request.Files["ImageFile"] != null)
+            {
+                var uniquename = string.Empty;
+                var Imgfile = Request.Files["ImageFile"];
+
+                if (Imgfile.FileName != "")
+                {
+                    string subPath = string.Format("/assets/DoctorImage/ScheduleImage/");
+                    bool exists = System.IO.Directory.Exists(Server.MapPath(subPath));
+
+                    if (!exists)
+                        System.IO.Directory.CreateDirectory(Server.MapPath(subPath));
+
+                    var ext = System.IO.Path.GetExtension(Imgfile.FileName);
+                    uniquename = Guid.NewGuid().ToString() + ext;
+
+                    var rootpath = Server.MapPath(string.Format("/assets/DoctorImage/ScheduleImage/"));
+                    string iconFileSavePath = System.IO.Path.Combine(rootpath, uniquename);
+                    s.ImageFile = string.Format("/assets/DoctorImage/ScheduleImage/{0}", uniquename);
+                    s.Image = string.Format("/assets/DoctorImage/ScheduleImage/{0}", uniquename);
+
+                    Imgfile.SaveAs(iconFileSavePath);
+                    db.Entry(s).State = EntityState.Modified;
+                    int c = db.SaveChanges();
+                    if (c > 0)
+                    {
+                        return Json(data: 1);
+                    }
+                }
+            }
+
+            else
+            {
+                return Json(new { data = 0, error = "No file uploaded" });
+            }
+
+            return Json(new { data = 0 });
+        }
+
+
+
+        [HttpPost]
+        public ActionResult DeleteSchedule(int id)
+        {
+            if (id > 0)
+            {
+                try
+                {
+                    var ScheduleIdRow = db.Schedules.FirstOrDefault(model => model.ScheduleID == id);
+                    if (ScheduleIdRow != null)
+                    {
+                        db.Entry(ScheduleIdRow).State = EntityState.Deleted;
+                        int a = db.SaveChanges();
+
+                        if (a > 0)
+                        {
+                            return Json(data: 1);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return Json(data: 0);
+
+        }
     }
 }
