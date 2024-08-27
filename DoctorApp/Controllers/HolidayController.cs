68f12882144704d0ae491fe9ada46cf2bbbdb34b
Husnain Memon <husnainraza61@gmail.com>
2024-08-27 04:09:17 -0700

    Complete Work

--- DoctorApp/Controllers/HolidayController.cs
+++ DoctorApp/Controllers/HolidayController.cs
@@ -39,5 +39,60 @@ namespace DoctorApp.Controllers
                 return Json(new { success = false, message = "Error occurred while adding the Role." });
             }
         }
+
+
+        public ActionResult EditHoliday(int id)
+        {
+            var row = db.Holidays.Where(model => model.HolidayID == id).FirstOrDefault();
+            return View(row);
+        }
+        [HttpPost]
+        public JsonResult EditHoliday(Holiday h)
+        {
+
+            db.Entry(h).State = EntityState.Modified;
+            int a = db.SaveChanges();
+            if (a > 0)
+            {
+                return Json(data: 1);
+            }
+            else
+            {
+                return Json(data: 0);
+
+            }
+
+        }
+
+
+        [HttpPost]
+        public ActionResult DeleteHoliday(int id)
+        {
+            if (id > 0)
+            {
+                try
+                {
+                    var HolidayIdRow = db.Holidays.FirstOrDefault(model => model.HolidayID == id);
+                    if (HolidayIdRow != null)
+                    {
+                        db.Entry(HolidayIdRow).State = EntityState.Deleted;
+                        int a = db.SaveChanges();
+
+                        if (a > 0)
+                        {
+                            return Json(data: 1);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Log the exception
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return Json(data: 0);
+
+        }
+
     }
 }
