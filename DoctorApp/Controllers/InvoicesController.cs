051f149ce117166a97d6ab780f5187ad9cb9748d
Husnain Memon <husnainraza61@gmail.com>
2024-09-11 03:52:09 -0700

    work Complete

---
+++ DoctorApp/Controllers/InvoicesController.cs
@@ -0,0 +1,21 @@
+﻿using DoctorApp.Models;
+using Omu.ValueInjecter;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace DoctorApp.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        DoctorClinicEntities db = new DoctorClinicEntities();
+        // GET: Invoices
+        public ActionResult Invoices()
+        {
+            return View();
+        }
+    }
+}
